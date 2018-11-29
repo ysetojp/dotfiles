478fe75857ac7313626c378465140403531088be
yseto <ysscmer@gmail.com>
2018-11-29 13:49:58 +0900

    modify ruby install

--- setup/linux/ruby_install.sh
+++ setup/linux/ruby_install.sh
@@ -1,2 +1,8 @@
-sudo apt-get -y install rbenv
-sudo apt-get -y install ruby-build
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+~/.rbenv/bin/rbenv init
+. ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+rbenv install -l
