cf4bbe8316bb4f5bfbc2e7a9e71eac7d12de4b4f
yseto <ysscmer@gmail.com>
2019-08-14 15:11:10 +0900

    update create_links

--- setup/common/create_links.sh
+++ setup/common/create_links.sh
@@ -4,10 +4,10 @@ mkdir -p ~/workspace         2>/dev/null
 mkdir -p ~/app               2>/dev/null
 mkdir -p ~/.fonts            2>/dev/null
 mkdir -p ~/.config/nvim/dein 2>/dev/null
-mkdir -p ~/.vim/backup       2>/dev/null
-mkdir -p ~/.vim/swap         2>/dev/null
+# mkdir -p ~/.vim/backup       2>/dev/null
+# mkdir -p ~/.vim/swap         2>/dev/null
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+# ln -s ~/dotfiles/.vimrc ~/.vimrc
 # ln -s ~/dotfiles/.gvimrc ~/.gvimrc
 # ln -s ~/dotfiles/.vim ~/.vim
 # ln -s ~/dotfiles/.vimwinpos ~/.vimwinpos
@@ -17,8 +17,8 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 # ln -s ~/dotfiles/.emacs.d ~/.emacs.d
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 #ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/.railsrc ~/.railsrc
-# ln -s ~/dotfiles/bin ~/bin
+# ln -s ~/dotfiles/.railsrc ~/.railsrc
+ln -s ~/dotfiles/bin ~/bin
 ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.config/nvim/colors ~/.config/nvim/colors
 ln -s ~/dotfiles/.config/nvim/dein/plugins.toml ~/.config/nvim/dein/plugins.toml
